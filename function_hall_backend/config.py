44bc899b03a8921303f8ee71fd86bd047ed1bf5d
geninfo133 <geninfo133@gmail.com>
2025-12-15 23:15:57 +0530

    backend

--- function_hall_backend/config.py
+++ function_hall_backend/config.py
@@ -1,6 +1,7 @@
-
+import os
 DB_USER = "postgres"
 DB_PASSWORD = "gen"
 DB_HOST = "localhost"
 DB_PORT = "5432"
 DB_NAME = "functionhall_db"
+SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
